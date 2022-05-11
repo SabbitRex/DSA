7c01ca44f9cbfb012ab96015272125d46f94d03f
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-12 00:09:43 +0700

    Create InOrderOfBT.java
    https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ Problems/InOrderOfBT.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class InOrderOfBT {
+    
+    List<Integer> list = new ArrayList<>();
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        
+        inOrder(root);
+        
+        return list;
+        
+    }
+    
+    public void inOrder(TreeNode root) {
+        
+        if (root == null) {
+            
+            return;
+        }
+        
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+}
