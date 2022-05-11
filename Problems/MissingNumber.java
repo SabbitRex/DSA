f432e692292b87f0c18961ca803d598786dd1250
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-11 12:23:18 +0700

    Create MissingNumber.java

---
+++ Problems/MissingNumber.java
@@ -0,0 +1,32 @@
+class MissingNumber {
+    
+    public int missingNumber(int[] nums) {
+        
+        int count = nums.length;
+        
+        int upperLimit = count;
+        
+        int missingNumber = 0;
+        
+        for (int i=upperLimit; i>0; i--) {
+            
+            boolean isPresent = false;
+            
+            for (int j=0; j<count; j++) {
+                    
+                if (nums[j] == i) {
+                    
+                    isPresent = true;
+                }
+            }
+            
+            if (!isPresent) {
+                
+                missingNumber = i;
+                break;
+            }
+        }
+        
+        return missingNumber;
+    }
+}
