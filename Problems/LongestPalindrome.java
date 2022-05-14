c1084a657b8290014d87549c76186024275831cc
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-15 02:38:17 +0700

    https://leetcode.com/problems/longest-palindromic-substring/
    Working for small string length, TLE for very very very large strings.

---
+++ Problems/LongestPalindrome.java
@@ -0,0 +1,70 @@
+public class LongestPalindrome {
+	
+    public static String longestPalindrome(String s) {
+        
+        if (s.length() == 1) {
+            
+           return s; 
+        }
+        
+        String[] str = s.split("");
+        
+        int globalMaxLength = 0;
+        
+        String result = null;
+        
+        for (int i=0; i<str.length; i++) {
+            
+            String temp = str[i];
+            
+            for (int j=i+1; j<str.length; j++) {
+                
+                temp = temp + str[j];
+                
+                boolean isPalindrome = checkPalindrome(temp);
+                
+                if (isPalindrome) {
+                	
+                	if (globalMaxLength < temp.length()) {
+                		
+                		result = temp;
+                		globalMaxLength = temp.length();
+                	}
+                }
+                
+                if (temp.length() == s.length() && temp.equals(s) && isPalindrome) {
+                	
+                	return temp;
+                }
+            }
+        }
+        
+        if (result == null) {
+            
+            return str[0];
+        }
+        
+        return result;
+    }
+    
+    private static boolean checkPalindrome(String str) {
+        
+    	String reverseStr = "";
+    	
+        for (int i = (str.length() - 1); i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+        
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+        	
+        	return true;
+        }
+        
+        return false;
+    }
+    
+    public static void main(String[] args) {
+		
+    	System.out.println(longestPalindrome("babad"));
+	}
+}
