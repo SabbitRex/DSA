0d5719636b01d2ae7c09a70a1221e2b3b084977a
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-20 22:15:29 +0700

    Create FirstAndLastPosition.java
    https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/

---
+++ Problems/FirstAndLastPosition.java
@@ -0,0 +1,36 @@
+class FirstAndLastPosition {
+
+    public int[] searchRange(int[] nums, int target) {
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        int[] result = new int[] {-1, -1};
+        
+        while (start <= end) {
+            
+            if (nums[start] == nums[end] && nums[start] == target) {
+                
+                result[0] =  start;
+                result[1] = end;
+                
+                break;
+                
+            } else if (nums[start] == target && nums[end] != target) {
+                
+                end--;
+                
+            } else if (nums[start] != target && nums[end] == target) {
+                
+                start++;
+                
+            } else {
+            	
+            	start++;
+            	end--;
+            }
+        }
+        
+        return result;
+    }
+}
