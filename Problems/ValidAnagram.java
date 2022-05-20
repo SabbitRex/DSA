4b0c449823226205e46830d46bc1c38929ebb2ca
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-21 05:28:40 +0700

    Create ValidAnagram.java
    https://leetcode.com/problems/valid-anagram/

---
+++ Problems/ValidAnagram.java
@@ -0,0 +1,31 @@
+class ValidAnagram {
+    
+    public boolean isAnagram(String s, String t) {
+        
+        int[] freqS1 = getFrequency(s);
+        
+        int[] freqS2 = getFrequency(t);
+        
+        for (int i=0; i<26; i++) {
+        	
+        	if (freqS1[i] != freqS2[i]) {
+        		
+        		return false;
+        	}
+        }
+        
+        return true;
+    }
+    
+    private int[] getFrequency(String str) {
+        
+        int[] frequency = new int[26];
+        
+        for (int i=0; i<str.length(); i++) {
+            
+            frequency[str.charAt(i) - 'a']++;
+        }
+        
+        return frequency;
+    }
+}
