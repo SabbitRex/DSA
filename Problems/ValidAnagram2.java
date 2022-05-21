32ab6f5d1790c74553aff1d40e5be4d7a51c9ed6
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-21 16:15:03 +0700

    Create ValidAnagram2.java
    Faster using Arrays.sort()

---
+++ Problems/ValidAnagram2.java
@@ -0,0 +1,20 @@
+class ValidAnagram2 {
+    
+    public boolean isAnagram(String s, String t) {
+            
+        char[] c1 = s.toCharArray();
+        
+        Arrays.sort(c1);
+        
+        char[] c2 = t.toCharArray();
+        
+        Arrays.sort(c2);
+        
+        if (new String(c1).equals(new String(c2))) {
+            
+            return true;
+        }
+        
+        return false;
+    }
+}
