f87ebc8b6e5dbe017884737bb5f1ce678db16ee9
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-25 12:41:20 +0530

    https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ Problems/MiddleOfLinkedList.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MiddleOfLinkedList {
+    
+    public ListNode middleNode(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast.next != null) {
+            
+            slow = slow.next;
+            
+            if (fast.next.next == null) {
+                
+                return slow;
+                
+            } else {
+                
+                fast = fast.next.next;
+            }
+            
+        }
+        
+        return slow;
+    }
+}
