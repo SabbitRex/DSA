ff5fd2c2af0d25e1ea48d28af340ef88bbe6d7b2
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-05-26 12:29:45 +0530

    https://leetcode.com/problems/missing-number/

---
+++ Problems/FindMissingNumber.java
@@ -0,0 +1,23 @@
+class FindMissingNumber {
+    
+    public int missingNumber(int[] nums) {
+        
+        int size = nums.length;
+        
+        int sum = 0;
+        
+        int sumNums = 0;
+        
+        for (int i=0; i<=size; i++) {
+            
+            sum = sum + i;
+            
+            if (i != size) {
+                
+                sumNums = sumNums + nums[i];
+            }
+        }
+        
+        return sum - sumNums;
+    }
+}
