629586c4d800e3bf0deabd2b8d9e9eb42c56823f
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-03 00:37:13 +0700

    Update LongestCommonSubsequenceBottomUp.java

--- Problems/LongestCommonSubsequenceBottomUp.java
+++ Problems/LongestCommonSubsequenceBottomUp.java
@@ -2,39 +2,27 @@ class LongestCommonSubsequenceBottomUp {
     
     public int longestCommonSubsequence(String text1, String text2) {
         
-        String[] str1 = text1.split("");
-        String[] str2 = text2.split("");
+        int m = text1.length();
         
-        int m = str1.length;
-        int n = str2.length;
+        int n = text2.length();
         
-        int[][] result = new int[m + 1][n + 1];
+        int[][] result = new int[m+1][n+1];
         
         for (int i=1; i<=m; i++) {
             
             for (int j=1; j<=n; j++) {
                 
-                if (str1[i-1].equals(str2[j-1])) {
+                if (text1.charAt(i-1) == text2.charAt(j-1)) {
                     
                     result[i][j] = 1 + result[i-1][j-1];
                     
                 } else {
                     
-                    result[i][j] = max(result[i-1][j], result[i][j-1]);
+                    result[i][j] = Math.max(result[i-1][j], result[i][j-1]);
                 }
             }
         }
         
         return result[m][n];
-    }
-    
-    private int max(int i, int j) {
-        
-        if (i < j) {
-            
-            return j;
-        }
-        
-        return i;
     }
 }
