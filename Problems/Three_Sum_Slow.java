73e769c6f5b306319d14cf7661f4c2b288f1cd3a
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-04 13:18:08 +0700

    Update and rename ThreeSumSlow.java to Three_Sum_Slow.java

--- Problems/Three_Sum_Slow.java
+++ Problems/Three_Sum_Slow.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class ThreeSum {
+public class Three_Sum_Slow {
 	
     public static List<List<Integer>> threeSum(int[] nums) {
         
