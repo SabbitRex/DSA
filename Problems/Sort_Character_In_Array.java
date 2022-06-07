285dbf4a059067e1da7dc9599019bfe43a29b26d
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-08 00:10:36 +0700

    Create Sort_Character_In_Array.java

---
+++ Problems/Sort_Character_In_Array.java
@@ -0,0 +1,31 @@
+class Sort_Characters_In_Array {
+
+	public List<Character> sort(char[] arr) {
+		
+		List<Character> result = new ArrayList<>();
+		
+		int[] freq = new int[26];
+		
+		for (char c : arr) {
+			
+			freq[c - 'a']++;
+		}
+		
+		for (int i=0; i<freq.length; i++) {
+			
+			if (freq[i] != 0) {
+				
+				while (freq[i] != 0) {
+					
+					char c = (char) (97 + i);
+					
+					result.add(c);
+					
+					freq[i]--;
+				}
+			}
+		}
+		
+		return result;
+    }
+}
