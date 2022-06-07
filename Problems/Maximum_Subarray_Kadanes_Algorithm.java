2a391803e025870480edd9056565ca4f56a394bd
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-07 15:35:48 +0700

    Update and rename KadaneAlgorithm.java to Maximum_Subarray_Kadanes_Algorithm.java

--- Problems/Maximum_Subarray_Kadanes_Algorithm.java
+++ Problems/Maximum_Subarray_Kadanes_Algorithm.java
@@ -1,4 +1,4 @@
-public class KadaneAlgorithm {
+public class Maximum_Subarray_Kadanes_Algorithm {
 
 	public static int maxSubArray(int[] arr) {
 		
@@ -24,14 +24,5 @@ public class KadaneAlgorithm {
 		}
 		
 		return globalMax;
-	}
-	
-	public static void main(String[] args) {
-
-		int[] arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-
-		//int[] arr = new int[] { 5, 4, -1, 7, 8 };
-
-		System.out.println(maxSubArray(arr));
 	}
 }
