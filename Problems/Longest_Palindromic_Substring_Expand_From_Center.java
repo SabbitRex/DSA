30535e1d8637bcd3212c3781085e2a4055864a2e
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-09 03:27:01 +0700

    Update and rename Longest_Palindromic_Substring_Expand_From_Centre.java to Longest_Palindromic_Substring_Expand_From_Center.java

--- Problems/Longest_Palindromic_Substring_Expand_From_Center.java
+++ Problems/Longest_Palindromic_Substring_Expand_From_Center.java
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/longest-palindromic-substring/
 
-class Longest_Palindromic_Substring_Expand_From_Centre {
+class Longest_Palindromic_Substring_Expand_From_Center {
     
     public String longestPalindrome(String s) {
         
