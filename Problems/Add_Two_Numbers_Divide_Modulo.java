c4653aecd1f5a6867c32a75228e0126cb28a7e84
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-13 19:59:02 +0700

    Update and rename AddTwoNumbers.java to Add_Two_Numbers_Divide_Modulo.java

--- Problems/Add_Two_Numbers_Divide_Modulo.java
+++ Problems/Add_Two_Numbers_Divide_Modulo.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class AddTwoNumbers {
+class Add_Two_Numbers_Divide_Modulo {
     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
