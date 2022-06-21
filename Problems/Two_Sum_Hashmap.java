800d23a13058bb3c407c9897a56c48dc5f38a426
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-21 13:51:59 +0530

    Update Two_Sum_Hashmap.java

--- Problems/Two_Sum_Hashmap.java
+++ Problems/Two_Sum_Hashmap.java
@@ -3,25 +3,27 @@
 class Two_Sum_Hashmap {
     
     public int[] twoSum(int[] nums, int target) {
-            
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         
-        int[] result = null;
+        int[] result = new int[2];
         
-        int idx = -1;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        int idx = 0;
         
         for (int num : nums) {
-            
-            idx++;
             
             if (map.containsKey(target - num)) {
                 
                 result = new int[] {idx, map.get(target - num)};
                 
+                break;
+                
             } else {
                 
                 map.put(num, idx);
             }
+            
+            idx++;
         }
         
         return result;
