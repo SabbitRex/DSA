b047fe0cf1cbfa14c02a5d4d4c2048f6b87e7968
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 20:08:00 +0700

    Create Valid_Palindrome.java

---
+++ Problems/String/Valid_Palindrome.java
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/valid-palindrome/
+
+class Valid_Palindrome {
+    
+    public boolean isPalindrome(String s) {
+        
+		s = s.replace(" ", "").toLowerCase();
+
+		if (s.length() == 1) {
+
+			return true;
+		}
+
+		int f = 0;
+
+		int r = s.length() - 1;
+
+		while (f < r) {
+
+			char front = s.charAt(f);
+
+			char rear = s.charAt(r);
+
+			while (!((front >= 'a' && front <= 'z') || (front >= '0' && front <= '9')) && f < r) {
+                
+				f++;
+				front = s.charAt(f);
+			}
+
+			while (!((rear >= 'a' && rear <= 'z') || (rear >= '0' && rear <= '9')) && r > f) {
+                
+				r--;
+				rear = s.charAt(r);
+			}
+
+			if (front != rear) {
+                
+				return false;
+			}
+
+			f++;
+			r--;
+		}
+
+		return true;
+    }
+}
