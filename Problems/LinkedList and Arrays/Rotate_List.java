0cbb440eff91ca6ba64234bd10045dfcd57241ad
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:25:45 +0700

    Create Rotate_List.java

---
+++ Problems/LinkedList and Arrays/Rotate_List.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+ # https://leetcode.com/problems/rotate-list/
+ 
+class Rotate_List {
+    
+    public ListNode rotateRight(ListNode head, int k) {
+        
+		if (head == null || head.next == null) {
+			
+			return head;
+		}
+		
+		ListNode current = head;
+		int size = 2;
+		
+		while (current.next.next != null) {
+			
+			current = current.next;
+			size++;
+		}
+		
+		k = k % size;
+		
+		if ( k == 0) {
+			
+			return head;
+		}
+		
+		ListNode root = current.next;
+		root.next = head;
+		current.next = null;
+		
+		return rotateRight(root, --k);
+    }
+}
