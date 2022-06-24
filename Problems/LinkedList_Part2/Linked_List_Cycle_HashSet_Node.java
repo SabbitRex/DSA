9933c05bfd9a93aa6ffb941ff259fdd9165e92f3
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:18:55 +0700

    Create Linked_List_Cycle_HashSet_Node.java

---
+++ Problems/LinkedList_Part2/Linked_List_Cycle_HashSet_Node.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+ 
+public class Linked_List_Cycle_HashSet_Node {
+
+    public ListNode detectCycle(ListNode head) {
+        
+        ListNode node = null;
+        
+        Set<ListNode> set = new HashSet<ListNode>();
+        
+        if (head == null) {
+            
+            return null;
+        }
+        
+        while (head.next != null) {
+                
+            if (set.add(head)) {
+                
+                head = head.next;
+                
+            } else {
+                
+                node = head; 
+                break;
+            }
+        }
+        
+        return node;
+    }
+}
