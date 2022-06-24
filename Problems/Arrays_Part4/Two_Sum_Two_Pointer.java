a2c6add0d3f421a04132ab9c9ae1bbc66f826f0e
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:00:49 +0700

    Create Two_Sum_Two_Pointer.java

---
+++ Problems/Arrays_Part4/Two_Sum_Two_Pointer.java
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/two-sum/
+
+class Two_Sum_Two_Pointer {
+    
+    public int[] twoSum(int[] nums, int target) {
+        
+        int i = 0;
+        int j = i + 1;
+        
+        int size = nums.length;
+        
+        int[] result = new int[2];
+        
+        while (i < j) {
+            
+            int fixed = nums[i];
+            int moving = nums[j];
+            
+            int sum = fixed + moving;
+             
+            if (target == sum) {
+                
+                result[0] = i;
+                result[1] = j;
+                
+                break;
+                
+            } else {
+                
+                j++;
+            }
+            
+            if (j == size && i < size - 1) {
+                
+                i++;
+                j = i + 1;
+            }
+        }
+        
+        return result;
+    }
+}
