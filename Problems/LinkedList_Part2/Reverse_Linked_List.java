edcecc34e9e1d5e04fad9b1c81f30d52f61084c8
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:53:22 +0700

    Create Reverse_Linked_List.java

---
+++ Problems/LinkedList_Part2/Reverse_Linked_List.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+# https://leetcode.com/problems/reverse-linked-list-ii/
+
+class Reverse_Linked_List {
+    
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        
+        if (head == null) {
+            
+            return null;
+        }
+        
+        if (left == right) {
+            
+            return head;    
+        }
+        
+        ListNode dummy = new ListNode(0, head);
+        
+        ListNode prevLeft = dummy;
+        ListNode leftB = head;
+        
+        int idx = 1;
+        
+        while (idx < left) {
+            
+            prevLeft = leftB;
+            leftB = leftB.next;
+            idx++;
+        }
+        
+        ListNode temp = null;
+        
+        while (idx <= right) {
+            
+            ListNode nextLeft = leftB.next;
+            leftB.next = temp;
+            temp = leftB;
+            leftB = nextLeft;
+            idx++;
+        }
+        
+        prevLeft.next.next = leftB;
+        prevLeft.next = temp; 
+        
+        return dummy.next;
+    }
+}
