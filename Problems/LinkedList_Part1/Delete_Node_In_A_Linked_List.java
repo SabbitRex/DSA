ed51d9ce713ec5ee928471498376ebf19fac54ea
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:13:39 +0700

    Create Delete_Node_In_A_Linked_List.java

---
+++ Problems/LinkedList_Part1/Delete_Node_In_A_Linked_List.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// You are only given the node to delete and not the head.
+// It is guaranteed that the node to be deleted is not a tail node in the list.
+
+# https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Delete_Node_In_A_Linked_List {
+    
+    public void deleteNode(ListNode node) {
+        
+        if (node != null && node.next != null) {
+            
+            node.val = node.next.val;
+            
+            node.next = node.next.next;
+        }
+    }
+}
