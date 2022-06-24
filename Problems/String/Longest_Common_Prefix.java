cd3af7e8101d4c6defa26667e52604703ab98a95
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-24 19:35:21 +0700

    Create Longest_Common_Prefix.java

---
+++ Problems/String/Longest_Common_Prefix.java
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/longest-common-prefix/
+
+class Longest_Common_Prefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        
+        String longestCommonPrefix  = "";
+        
+        if (strs.length == 0 || strs == null) {
+            
+            return longestCommonPrefix;
+        }
+        
+        int index = 0;
+        
+        for (char chr : strs[0].toCharArray()) {
+            
+            for (int i=1; i<strs.length; i++) {
+                
+                if (index >= strs[i].length() || chr != strs[i].charAt(index)) {
+                    
+                    return longestCommonPrefix;
+                }
+            }
+            
+            longestCommonPrefix = longestCommonPrefix + chr;
+            index++;
+        }
+        
+        return longestCommonPrefix;
+    }
+}
