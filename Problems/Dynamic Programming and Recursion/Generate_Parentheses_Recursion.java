d7f376b8fe06e1b329c80ca0ab1233605134eebe
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-06-28 03:43:02 +0700

    Create Generate_Parentheses_Recursion.java

---
+++ Problems/Dynamic Programming and Recursion/Generate_Parentheses_Recursion.java
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/generate-parentheses/
+
+class Generate_Parentheses_Recursion {
+    
+    List<String> result = new ArrayList<String>();
+	
+	Stack<String> stack = new Stack<>();
+    
+    public List<String> generateParenthesis(int n) {
+        
+		generate(0, 0, n);
+		
+		return result;
+    }
+    
+    private void generate(int countOpen, int countClose, int freq) {
+		
+		if (countOpen ==  freq && countOpen == countClose && countClose == freq) {
+			
+			result.add(stack.toString()
+                .replace(",", "")
+                .replace(" ", "")
+                .replace("[", "")
+                .replace("]", ""));
+            
+			return;
+		}
+		
+		if (countOpen < freq) {
+			
+			stack.add("(");
+			generate(countOpen + 1, countClose, freq);
+			stack.pop();
+		}
+		
+		if (countClose < countOpen) {
+			
+			stack.add(")");
+			generate(countOpen, countClose + 1, freq);
+			stack.pop();
+		}
+	}
+}
