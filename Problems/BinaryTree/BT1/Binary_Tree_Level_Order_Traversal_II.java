32ee1bd75254a01126de08a7efa937e2b1d3b4d9
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-09 17:53:28 +0700

    Create Binary_Tree_Level_Order_Traversal_II.java

---
+++ Problems/BinaryTree/BT1/Binary_Tree_Level_Order_Traversal_II.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+
+class Binary_Tree_Level_Order_Traversal_II {
+    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        
+        if (root == null) {
+            
+            return new ArrayList<>();
+        }
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        queue.add(root);
+        
+        while (!queue.isEmpty()) {
+            
+            int queueSize = queue.size();
+            
+            List<Integer> local = new ArrayList<>();
+            
+            while (queueSize > 0) {
+                
+                TreeNode current = queue.poll();
+                
+                local.add(current.val);
+                
+                if (current.left != null) {
+                    
+                    queue.add(current.left);
+                }
+                
+                if (current.right != null) {
+                    
+                    queue.add(current.right);
+                }
+                
+                queueSize--;
+            }
+            
+            result.add(local);
+        }
+        
+        Collections.reverse(result);
+        
+        return result;
+    }
+}
