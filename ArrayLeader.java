719ead1534a2c1f4024d829bd7a797e49fd2a50a
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-11 01:13:53 +0700

    Update ArrayLeader.java

--- ArrayLeader.java
+++ ArrayLeader.java
@@ -7,7 +7,7 @@ public class ArrayLeader {
 		
 		for (int i=0; i<array.length-1; i++) {
 			
-			int currentNum = array[i];
+			int currentNum = array[i]; 
 			
 			boolean isCurrentLeader = true;
 			
