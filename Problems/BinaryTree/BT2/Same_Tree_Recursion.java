eab3e64868568546bdceab4bc496b4029e622a8e
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-13 17:21:10 +0700

    Create Same_Tree_Recursion.java

---
+++ Problems/BinaryTree/BT2/Same_Tree_Recursion.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+# https://leetcode.com/problems/same-tree/
+
+class Same_Tree_Recursion {
+  
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        boolean isSameTree = isOverlapping(p, q);
+        
+        return isSameTree;
+    }
+    
+    public boolean isOverlapping(TreeNode root1, TreeNode root2) {
+        
+        if (root1 == null && root2 == null) {
+            
+            return true;
+        }
+        
+        if (root1 == null && root2 != null || root1 != null && root2 == null) {
+            
+            return false;
+        }
+        
+        return root1.val == root2.val && isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+    }
+}
