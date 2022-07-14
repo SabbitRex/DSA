936f3d40bb7dd03b33fa52e296510214df3ddce6
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-15 03:51:29 +0700

    Create Predecessor_And_Successor_Recursive.java

---
+++ Problems/Binary Search Tree/BST 1/Predecessor_And_Successor_Recursive.java
@@ -0,0 +1,26 @@
+# https://practice.geeksforgeeks.org/problems/predecessor-and-successor/1#
+
+class Predecessor_And_Successor_Recursive {
+    
+    public static void findPreSuc(Node root, Res p, Res s, int key) {
+        
+        if(root==null) {
+            
+            return;
+        }
+        
+       findPreSuc(root.left, p, s, key);
+       
+       if(root.data < key) {
+           
+           p.pre = root;
+       }
+       
+       if(root.data > key && s.succ == null) {
+           
+           s.succ=root;
+       }
+       
+       findPreSuc(root.right, p, s, key);
+    }
+}
