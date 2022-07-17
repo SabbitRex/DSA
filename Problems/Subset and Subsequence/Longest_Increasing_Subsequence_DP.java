087c52cd8c23b124b7915db17db778e08f82f438
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-17 18:53:30 +0700

    Update Longest_Increasing_Subsequence_DP.java

--- Problems/Subset and Subsequence/Longest_Increasing_Subsequence_DP.java
+++ Problems/Subset and Subsequence/Longest_Increasing_Subsequence_DP.java
@@ -8,27 +8,24 @@ class Longest_Increasing_Subsequence_DP {
         
         for (int i=0; i<dp.length; i++) {
             
-            dp[i] = 1;
+            dp[i] = 1;    
         }
+        
+        int globalMax = 1;
         
         for (int i=1; i<nums.length; i++) {
             
             for (int j=0; j<i; j++) {
                 
-                if (nums[j] < nums[i]) {
+                if (nums[i] > nums[j]) {
                     
                     dp[i] = Math.max(dp[i], dp[j] + 1);
+                    
+                    globalMax = Math.max(dp[i], globalMax);
                 }
             }
         }
         
-        int max = 0;
-        
-        for (int i=0; i<dp.length; i++) {
-            
-            max = Math.max(max, dp[i]);
-        }
-        
-        return max;
+        return globalMax;
     }
 }
