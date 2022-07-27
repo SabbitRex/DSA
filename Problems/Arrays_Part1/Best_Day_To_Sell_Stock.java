2b91e81dad2af434d090f61ea83fb74402aa2f7d
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-07-27 23:46:18 +0700

    Update Best_Day_To_Sell_Stock.java

--- Problems/Arrays_Part1/Best_Day_To_Sell_Stock.java
+++ Problems/Arrays_Part1/Best_Day_To_Sell_Stock.java
@@ -1,39 +1,34 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
 class Best_Day_To_Sell_Stock {
     
     public int maxProfit(int[] p) {
         
-        int globalProfit = Integer.MIN_VALUE;
-        
-        int i = 0;
-        int j = 1;
-        
-        boolean transactionDone = false;
-        
-        while (j < p.length) {
-            
-            int localProfit = p[j] - p[i];
-            
-            if (p[i] > p[j]) {
-                
-                i = j;
-                j = i+1;
-                
-            } else {
-                
-                j++;
-            }
-            
-            if (localProfit > globalProfit) {
-                
-                globalProfit = localProfit;
-            }
-        }
-        
-        if (globalProfit < 0) {
-            
-            return 0;
-        }
-        
-        return globalProfit;
+		int globalMax = Integer.MIN_VALUE;
+		
+		int ptr = 1;
+		
+		int min = p[0];
+		
+		while (ptr < p.length) {
+			
+			int localProfit = p[ptr] - min;
+			
+			if (min > p[ptr]) {
+				
+				min = p[ptr];
+			}
+			
+			ptr++;
+			
+			globalMax = Math.max(globalMax, localProfit);
+		}
+		
+		if (globalMax < 0) {
+			
+			return 0;
+		}
+		
+		return globalMax;
     }
 }
