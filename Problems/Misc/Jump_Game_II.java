b7f9cc01284ceab600af91d690654787e8350250
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-08-07 18:50:27 +0700

    Create Jump_Game_II.java

---
+++ Problems/Misc/Jump_Game_II.java
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/jump-game-ii/
+
+class Jump_Game_II {
+    
+    public int jump(int[] nums) {
+        
+        int result = 0;
+        
+        int L = 0;
+        int R = 0;
+        
+        while (R < nums.length - 1) {
+            
+            int localMaxRight = 0;
+            
+            for (int i=L; i<=R; i++) {
+                
+                localMaxRight = Math.max(i + nums[i], localMaxRight);
+            }
+            
+            L = R + 1;
+            R = localMaxRight;
+            result++;         
+        }
+        
+        return result;
+    }
+}
