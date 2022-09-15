19349e79ef05548e864fd627af64f7595a72804f
Sharad Dutta <sharadduttaofficial@gmail.com>
2022-09-15 14:35:49 +0700

    Update Best_Day_To_Sell_Stock.java

--- Problems/Arrays_Part1/Best_Day_To_Sell_Stock.java
+++ Problems/Arrays_Part1/Best_Day_To_Sell_Stock.java
@@ -4,31 +4,29 @@ class Best_Day_To_Sell_Stock {
     
     public int maxProfit(int[] p) {
         
-		int globalMax = Integer.MIN_VALUE;
-		
-		int ptr = 1;
-		
-		int min = p[0];
-		
-		while (ptr < p.length) {
-			
-			int localProfit = p[ptr] - min;
-			
-			if (min > p[ptr]) {
-				
-				min = p[ptr];
-			}
-			
-			ptr++;
-			
-			globalMax = Math.max(globalMax, localProfit);
-		}
-		
-		if (globalMax < 0) {
+        int globalMaxProfit = Integer.MIN_VALUE;
+        
+        int localMaxProfit = 0;
+        
+        int min = prices[0];
+        
+        for (int i=1; i<prices.length; i++) {
+            
+            localMaxProfit = prices[i] - min;
+            
+            if (min > prices[i]) {
+                
+                min = prices[i];
+            }
+            
+            globalMaxProfit = Math.max(globalMaxProfit, localMaxProfit);
+        }
+        
+        if (globalMaxProfit < 0) {
 			
 			return 0;
 		}
-		
-		return globalMax;
+        
+        return globalMaxProfit;
     }
 }
