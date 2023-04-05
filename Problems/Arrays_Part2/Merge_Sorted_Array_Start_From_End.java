df700b3716106b5ecccaa09c2acc46615de7e356
Sharad Dutta <sharadduttaofficial@gmail.com>
2023-04-06 04:44:34 +0700

    Update Merge_Sorted_Array_Start_From_End.java

--- Problems/Arrays_Part2/Merge_Sorted_Array_Start_From_End.java
+++ Problems/Arrays_Part2/Merge_Sorted_Array_Start_From_End.java
@@ -3,44 +3,30 @@
 class Merge_Sorted_Array_Start_From_End {
     
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    
-        // Start filling from last pos of nums1
-        int insertPosition = m + n - 1;
         
-        int idx1 = m-1;
-        int idx2 = n-1;
+        int positionToAdd = nums1.length - 1;
         
-        // Fill till one of nums1 or nums2 exhausts and keep decrementing last pos
-        while (idx1 >= 0 && idx2 >= 0) {
+        m = m-1;
+        n = n-1;
+        
+        while (m>=0 && n>=0) {
             
-            if (nums1[idx1] < nums2[idx2]) {
-                
-                nums1[insertPosition] = nums2[idx2];
-
-                idx2--;
-                
-            } else if (nums1[idx1] > nums2[idx2]) {
-                
-                nums1[insertPosition] = nums1[idx1];
-  
-                idx1--;
+            if (nums1[m] < nums2[n]) {
+                nums1[positionToAdd] = nums2[n];
+                n--;
                 
             } else {
-                
-                nums1[insertPosition] = nums1[idx1];
-
-                idx1--;
+                nums1[positionToAdd] = nums1[m];
+                m--;
             }
             
-            insertPosition--;
+            positionToAdd--;
         }
         
-        // Fill left over of nums2 to nums1 incase there is any
-        while (idx2 >= 0) {
-            
-            nums1[insertPosition] = nums2[idx2];
-            idx2--;
-            insertPosition--;
+        while (n >= 0) {
+            nums1[positionToAdd] = nums2[n];
+            n--;
+            positionToAdd--;
         }
     }
 }
